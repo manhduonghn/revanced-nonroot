ce30d9dc555cbf7a262fb2812d32fc06b02f181b
manhduonghn <manhduonghn@live.com>
2024-08-29 09:12:21 +0700

    Skip patch

--- revanced.sh
+++ revanced.sh
@@ -6,21 +6,27 @@ source ./etc/utils.sh
 # Perform download_repository_assets
 download_resources
 
-# Patch YouTube 
-apkmirror "google-inc" \
-          "youtube" \
-          "com.google.android.youtube"
-apply_patches "youtube"
-sign_patched_apk "youtube"
-create_github_release "youtube"
+ls revanced-patches*.jar > current_file.txt
 
-# Patch YouTube Music 
-apkmirror "google-inc" \
-          "youtube-music" \
-          "com.google.android.apps.youtube.music" \
-          "arm64-v8a"
-apply_patches "youtube-music"
-sign_patched_apk "youtube-music"
-create_github_release "youtube-music"
+if cmp -s current_file.txt patches.txt; then
+    echo "No change, skipping patch..."
+else
+    rm patches.txt > /dev/null 2>&1
+    # Patch YouTube
+    apkmirror "google-inc" \
+              "youtube" \
+              "com.google.android.youtube"
+    apply_patches "youtube"
+    sign_patched_apk "youtube"
+    create_github_release "youtube"
 
-# You can add other apps here 
+    # Patch YouTube Music 
+    apkmirror "google-inc" \
+              "youtube-music" \
+              "com.google.android.apps.youtube.music" \
+              "arm64-v8a"
+    apply_patches "youtube-music"
+    sign_patched_apk "youtube-music"
+    create_github_release "youtube-music"
+    mv current_file.txt patches.txt
+fi
