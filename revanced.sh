3b16624a7f4763eefccad6911098d7697c2ec8d7
manhduonghn <manhduonghn@live.com>
2024-10-22 06:56:33 +0700

    Fix

--- revanced.sh
+++ revanced.sh
@@ -22,6 +22,32 @@ patch_upload() {
     create_github_release "$app_name"
 }
 
+# Function to fetch the latest release version of a GitHub repository
+get_latest_release_version() {
+    local repo="$1"
+    local url="https://api.github.com/repos/${repo}/releases/latest"
+
+    # Use req to get the latest release tag name, including the GitHub token in the header
+    response=$(req - --header="Authorization: token $GITHUB_TOKEN" "$url" 2>/dev/null)
+
+    # Check if the request was successful
+    if [[ $? -eq 0 ]]; then
+        # Extract the tag name from the response
+        tag_name=$(echo "$response" | grep -oP '"tag_name":\s*"\K(v?[\d.]+)' | head -n 1)
+
+        if [[ -n "$tag_name" ]]; then
+            # Extract the version from the tag (e.g., v4.16.0-release to 4.16.0)
+            echo "$tag_name" | grep -oP '\d+\.\d+\.\d+'
+        else
+            echo "Error: Tag name not found for $repo"
+            return 1
+        fi
+    else
+        echo "Error: Failed to fetch release version for $repo"
+        return 1
+    fi
+}
+
 # Function to compare versions of two repositories
 compare_repository_versions() {    
     version_patches=$(get_latest_release_version "ReVanced/revanced-patches")
