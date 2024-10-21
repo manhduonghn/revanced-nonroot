2b3c07aad63228e8c455d383c83d43edbc6f97bf
manhduonghn <manhduonghn@live.com>
2024-10-22 06:52:21 +0700

    Use req

--- revanced.sh
+++ revanced.sh
@@ -22,32 +22,6 @@ patch_upload() {
     create_github_release "$app_name"
 }
 
-# Function to fetch the latest release version of a GitHub repository
-get_latest_release_version() {
-    local repo="$1"
-    local url="https://api.github.com/repos/${repo}/releases/latest"
-    
-    # Use curl to get the latest release tag name
-    response=$(curl -s -H "Authorization: token $GITHUB_TOKEN" "$url")
-    
-    # Check if the request was successful
-    if [[ $? -eq 0 ]]; then
-        # Extract the tag name from the response
-        tag_name=$(echo "$response" | grep -oP '"tag_name":\s*"\K(v?[\d.]+)' | head -n 1)
-        
-        if [[ -n "$tag_name" ]]; then
-            # Extract the version from the tag (e.g., v4.16.0-release to 4.16.0)
-            echo "$tag_name" | grep -oP '\d+\.\d+\.\d+'
-        else
-            echo "Error: Tag name not found for $repo"
-            return 1
-        fi
-    else
-        echo "Error: Failed to fetch release version for $repo"
-        return 1
-    fi
-}
-
 # Function to compare versions of two repositories
 compare_repository_versions() {    
     version_patches=$(get_latest_release_version "ReVanced/revanced-patches")
@@ -65,12 +39,10 @@ compare_repository_versions() {
     fi
 }
 
-
 # Compare versions
 if ! compare_repository_versions "$repo_patches" "$repository"; then
     echo "Running build..."
     download_resources
     patch_upload "youtube"
     patch_upload "youtube-music"
-    delete_cache
 fi
